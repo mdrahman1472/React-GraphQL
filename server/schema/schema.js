bcf5223dd545db3bd79363ed9ae573fb8739a583
Md Rahman <mdrahman1472@yahoo.com>
2020-02-21 18:52:36 -0700

    added author into book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,9 +11,9 @@ const {
 
 // dummy data
 let books = [
-   { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-   { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+   { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
 ];
 
 let authors = [
@@ -27,7 +27,13 @@ const BookType = new GraphQLObjectType({
    fields: () => ({ // having function instead of just object beacuse it will have reference error when we add some other thing that is defined in other place and not defined before this has called
       id: { type: GraphQLID },
       name: { type: GraphQLString },
-      genre: { type: GraphQLString }
+      genre: { type: GraphQLString },
+      author: {
+         type: AuthorType,
+         resolve(parent, args) {
+            return _.find(authors, { id: parent.authorId})
+         } 
+      }
    })
 });
 
